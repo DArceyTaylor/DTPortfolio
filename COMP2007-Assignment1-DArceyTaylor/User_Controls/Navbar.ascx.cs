c31f4fb40acc3175fde3503acb98c66a20ec4843
DArceyTaylor <darceytaylor05@gmail.com>
2016-05-27 11:08:55 -0400

    Add project files.

---
+++ COMP2007-Assignment1-DArceyTaylor/User_Controls/Navbar.ascx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+/**
+ * @author: D'Arcey Taylor
+ * @date: May 26, 2015
+ * @version: 0.0.1 added SetActivePage method
+**/
+namespace COMP2007_Assignment1_DArceyTaylor
+{
+    public partial class Navbar : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+        /**
+         * This method adds a css class of "active" to list items
+         * relating to the current page
+         * 
+         * @private
+         * @method SetActivePage
+         * @return {void}
+        **/
+        private void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    Home.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    Contact.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
